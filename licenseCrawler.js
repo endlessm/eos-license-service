5d0c877751d5566da8d8375fd39f8d585b4b3777
Will Thompson <wjt@endlessm.com>
2017-07-18 18:27:09 +0100

    Consistently indent with spaces

--- licenseCrawler.js
+++ licenseCrawler.js
@@ -49,27 +49,27 @@ function listPackages() {
 
 function getLicenseList(msg) {
     const htmlMeta =
-	'<meta charset="UTF-8">';
+        '<meta charset="UTF-8">';
     const htmlStyle =
-	'<style>\n' +
-	'body{margin:50px;min-width:630px;font-family: sans-serif;}\n' +
-	'h2{border-top:5px solid #4a4a4a;padding-top:20px;font-size:28px;line-height:34px;margin-top:70px;}\n' +
-	'h3{border-top:solid 1px #d8d8d8;padding-top:20px;font-size:22px;line-height:26px;clear:both;margin-top:50px;}\n' +
-	'p{font-size:18px;line-height:28px;}\n' +
-	'.copyright { font-family: monospace; font-size: initial; font-weight: initial; -moz-tab-size: 4; tab-size: 4; white-space: pre-wrap; }\n' +
-	'</style>\n';
+        '<style>\n' +
+        'body{margin:50px;min-width:630px;font-family: sans-serif;}\n' +
+        'h2{border-top:5px solid #4a4a4a;padding-top:20px;font-size:28px;line-height:34px;margin-top:70px;}\n' +
+        'h3{border-top:solid 1px #d8d8d8;padding-top:20px;font-size:22px;line-height:26px;clear:both;margin-top:50px;}\n' +
+        'p{font-size:18px;line-height:28px;}\n' +
+        '.copyright { font-family: monospace; font-size: initial; font-weight: initial; -moz-tab-size: 4; tab-size: 4; white-space: pre-wrap; }\n' +
+        '</style>\n';
     const htmlHeader =
-	'<html>\n' +
-	'<head>\n' + 
-	htmlMeta + 
-	htmlStyle + 
-	'</head>\n' +
-	'<body>\n' +
-	'<h2>Open Source Software</h2>\n';
-	// Happy preamble of legal compliance goes here...
+        '<html>\n' +
+        '<head>\n' +
+        htmlMeta +
+        htmlStyle +
+        '</head>\n' +
+        '<body>\n' +
+        '<h2>Open Source Software</h2>\n';
+        // Happy preamble of legal compliance goes here...
     const htmlFooter =
-	'</body>\n' +
-	'</html>';
+        '</body>\n' +
+        '</html>';
 
     // send the HTML header
     msg.status_code = 200;
@@ -82,22 +82,22 @@ function getLicenseList(msg) {
 
     packageNames.sort();
     packageNames.forEach(function(packageName) {
-	const copyrightPath = GLib.build_filenamev([CRAWL_LOCATION, packageName, 'copyright']);
+        const copyrightPath = GLib.build_filenamev([CRAWL_LOCATION, packageName, 'copyright']);
 
         try {
-	    const [success, copyrightContents] = GLib.file_get_contents(copyrightPath);
+            const [success, copyrightContents] = GLib.file_get_contents(copyrightPath);
         } catch (e if e.matches(GLib.FileError, GLib.FileError.NOENT)) {
             return;
         } catch (e) {
-	    logError(e, 'Unable to read copyright file ' + copyrightPath);
+            logError(e, 'Unable to read copyright file ' + copyrightPath);
             return;
         }
 
-	const dirNames = packages[packageName];
+        const dirNames = packages[packageName];
 
-	// send HTML for this directory
+        // send HTML for this directory
         try {
-	    const html = prepareHtml(dirNames, copyrightContents.toString());
+            const html = prepareHtml(dirNames, copyrightContents.toString());
             msg.response_body.append(html);
         } catch (e) {
             logError(e, 'Unable to convert contents of ' + copyrightPath + ' to string');
